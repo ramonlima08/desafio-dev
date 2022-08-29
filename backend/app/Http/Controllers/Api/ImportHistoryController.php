1d972fe45131dda0179f97284ac2c9a5bdf9e577
Ramon Lima <ramonlima08@gmail.com>
2022-08-29 08:49:55 -0300

    add method to revert imports

--- backend/app/Http/Controllers/Api/ImportHistoryController.php
+++ backend/app/Http/Controllers/Api/ImportHistoryController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\ImportHistory;
 use App\Models\Transaction;
 use App\Traits\ApiResponserTrait;
-use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
@@ -27,6 +26,45 @@ class ImportHistoryController extends Controller
         return $this->successResponse($importHistory);
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function toReverse(Request $request)
+    {
+        
+        try {
+            $messages  = [
+                'id.required' => 'O Id é obrigatório',
+            ];
     
+            $validator = Validator::make($request->all(), [
+                'id' => 'required',
+            ], $messages);
+    
+            if ($validator->fails()) 
+                return $this->errorResponse(['type'=>'validator', 'messages'=>$validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+            
+            DB::beginTransaction();
+
+            $importHistory = ImportHistory::FindOrFail($request->id);
+
+            //excluindo as transações
+            Transaction::where('import_history_id', $importHistory->id)->delete();
+
+            $importHistory->delete();
+
+            DB::commit();
+            return $this->successResponse([], Response::HTTP_CREATED);
+
+        } catch (\Throwable $th) {
+            DB::rollback();
+            Log::error("ImportHistoryController - toReverse - " . $th->getMessage());
+            return $this->errorResponse($th->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
